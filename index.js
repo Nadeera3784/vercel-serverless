13ff36d15557d3c10822081300e6105a077db08f
Nadeera Sampath <nadeera3784@gmail.com>
2023-09-15 18:01:15 +0530

    fix: code clean up

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 
-
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
 app.set('view engine', 'ejs');
 
@@ -29,7 +11,6 @@ app.get('/', function(req, res) {
   res.render('pages/home');
 });
 
-//app.use(express.static(__dirname + '/public'));
 
 app.get('/api/home', (req, res) => {
   res.status(200).json('Welcome to Home page API route'+  process.env.VERCEL_URL);
